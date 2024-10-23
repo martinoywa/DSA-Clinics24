bbd4e1fc244c439dbc8df19743c8a823b99d77ad
Martin Oywa <martinoywa2@gmail.com>
2024-10-24 00:27:08 +0300

    Update C3

--- martinoywa/C3.py
+++ martinoywa/C3.py
@@ -2,9 +2,14 @@ from math import log2
 
 
 def power_of_two(n):
+    # edge case
+    if n <= 0:
+        return False
     return log2(n) == int(log2(n))
 
 
 if __name__ == "__main__":
+    print(f"-1 => {power_of_two(-1)}")
+    print(f"0 => {power_of_two(0)}")
     print(f"8 => {power_of_two(8)}")
     print(f"6 => {power_of_two(6)}")
